ce8e71d671292bb534d86759376000a4727cea18
prebird <78974381+prebird@users.noreply.github.com>
2024-04-26 21:04:02 +0900

    add simple-payment-app project

---
+++ simple-payment-app/src/main/java/com/example/simplepaymentapp/SimplePaymentAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplepaymentapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplePaymentAppApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SimplePaymentAppApplication.class, args);
+  }
+
+}
---
+++ simple-payment-app/src/test/java/com/example/simplepaymentapp/SimplePaymentAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplepaymentapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplePaymentAppApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
