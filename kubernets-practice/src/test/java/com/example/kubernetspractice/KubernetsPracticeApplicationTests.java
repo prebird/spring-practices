d26e1c4999ec983f1e68f714b3b47569bf2b0125
prebird <78974381+prebird@users.noreply.github.com>
2024-05-04 14:53:51 +0900

    add kubernets-practice project

---
+++ kubernets-practice/src/test/java/com/example/kubernetspractice/KubernetsPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kubernetspractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubernetsPracticeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
