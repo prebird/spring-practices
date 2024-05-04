2a0044dd3cab7384ba164f1e0f0b1a56d8027b36
prebird <78974381+prebird@users.noreply.github.com>
2024-05-04 14:57:10 +0900

    refactor : 프로젝트명 변경 kubernets -> kubernetes

--- kubernetes-practice/src/main/java/com/example/kubernetespractice/KubernetesPracticeApplication.java
+++ kubernetes-practice/src/main/java/com/example/kubernetespractice/KubernetesPracticeApplication.java
@@ -1,13 +1,13 @@
-package com.example.kubernetspractice;
+package com.example.kubernetespractice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KubernetsPracticeApplication {
+public class KubernetesPracticeApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(KubernetsPracticeApplication.class, args);
+    SpringApplication.run(KubernetesPracticeApplication.class, args);
   }
 
 }
