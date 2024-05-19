38813c783acbf66adfa98726190c37a39c479a10
prebird <78974381+prebird@users.noreply.github.com>
2024-05-19 13:31:54 +0900

    [#2] deploy.yaml 에 환경변수를 전달하여 pod 생성하기

--- kubernetes-practice/src/main/java/com/example/kubernetespractice/main/MainController.java
+++ kubernetes-practice/src/main/java/com/example/kubernetespractice/main/MainController.java
@@ -4,6 +4,7 @@ import com.example.kubernetespractice.main.board.BoardDto;
 import com.example.kubernetespractice.main.board.BoardService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MainController {
   private final BoardService boardService;
+  @Value("${app.deployer.name}")
+  private String deployerName;
 
   @GetMapping("/health")
   public String main() {
@@ -21,5 +24,10 @@ public class MainController {
   @GetMapping("/boards")
   public ResponseEntity<List<BoardDto>> getAllBoards() {
     return ResponseEntity.ok(boardService.getBoards());
+  }
+
+  @GetMapping("/deployer")
+  public ResponseEntity<String> getDeployer() {
+    return ResponseEntity.ok(deployerName);
   }
 }
