64eed8772e6974b6ac0d38b5a52a97e61d622739
prebird <78974381+prebird@users.noreply.github.com>
2024-06-01 21:50:47 +0900

    [#4] httpExchages 엔드포인트

--- actuator/src/main/java/com/example/actuator/ActuatorApplication.java
+++ actuator/src/main/java/com/example/actuator/ActuatorApplication.java
@@ -1,7 +1,10 @@
 package com.example.actuator;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.audit.InMemoryAuditEventRepository;
+import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ActuatorApplication {
@@ -10,4 +13,9 @@ public class ActuatorApplication {
 		SpringApplication.run(ActuatorApplication.class, args);
 	}
 
+	@Bean
+	public InMemoryHttpExchangeRepository httpExchangeRepository() {
+		// 최대 요청수 100개, 개발 단계에서 사용하면 편함, 운영에서는 모니터링 툴, 핀포인트, zipkin 등 다른 기술을 사용하는 것이 좋다.
+		return new InMemoryHttpExchangeRepository();
+	}
 }
