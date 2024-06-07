1f7a862bfe56c7bf3ce18faf6bd67289c2f7dab1
prebird <78974381+prebird@users.noreply.github.com>
2024-06-07 09:21:42 +0900

    [#5] Serializable 구현 및 프로메테우스 의존성 추가, weight 추가

--- cache-practice/src/main/java/com/example/cachepractice/food/cache/CaffeineCacheConfig.java
+++ cache-practice/src/main/java/com/example/cachepractice/food/cache/CaffeineCacheConfig.java
@@ -1,18 +1,26 @@
 package com.example.cachepractice.food.cache;
 
+import com.example.cachepractice.food.dto.FoodDto;
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.RemovalCause;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.binder.cache.CaffeineStatsCounter;
 import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@RequiredArgsConstructor
 @Slf4j
-//@Configuration
+@Configuration
 public class CaffeineCacheConfig {
+  private final MeterRegistry meterRegistry;
   @Bean
   public CacheManager cacheManager() {
     CaffeineCacheManager cacheManager = new CaffeineCacheManager();
@@ -22,9 +30,20 @@ public class CaffeineCacheConfig {
         .expireAfterWrite(1, TimeUnit.SECONDS);
     cacheManager.setCaffeine(caffeine);
 
-    // custom 설정 등록
+//     custom 설정 등록
     Arrays.stream(CaffeineCacheEnum.values()).forEach(
         cacheEnum -> cacheManager.registerCustomCache(cacheEnum.getName(), cacheEnum.getCache()));
+
+    ;
+    // 카페인 캐시 추가 메트릭 설정
+    cacheManager.registerCustomCache("allFoods", Caffeine.newBuilder()
+        .recordStats(() -> new CaffeineStatsCounter(meterRegistry, "allFoods")) // 카페인 캐시 추가 메트릭 설정
+        .expireAfterWrite(60, TimeUnit.SECONDS)
+        .evictionListener((Object key, Object value,
+            RemovalCause cause) ->
+            log.info(String.format(
+                "Key %s was evicted (%s)%n", key, cause)))
+        .build());
 
     return cacheManager;
   }
--- cache-practice/src/main/java/com/example/cachepractice/food/cache/CaffeineCacheEnum.java
+++ cache-practice/src/main/java/com/example/cachepractice/food/cache/CaffeineCacheEnum.java
@@ -1,8 +1,12 @@
 package com.example.cachepractice.food.cache;
 
+import com.example.cachepractice.food.dto.FoodDto;
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import com.github.benmanes.caffeine.cache.RemovalCause;
+import io.micrometer.core.instrument.binder.cache.CaffeineStatsCounter;
+import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -16,11 +20,12 @@ public enum CaffeineCacheEnum {
       Caffeine.newBuilder()
       .recordStats()
       .expireAfterWrite(60, TimeUnit.SECONDS)
-      .maximumSize(100)
       .evictionListener((Object key, Object value,
           RemovalCause cause) ->
           log.info(String.format(
               "Key %s was evicted (%s)%n", key, cause)))
+      .weigher((String key, List<FoodDto> value) -> value.size())
+      .maximumWeight(100)
       .build());
 
   private final String name;
