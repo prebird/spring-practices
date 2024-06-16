d7b1f28ad76386645762bc851a3bbc943aaebd1d
prebird <78974381+prebird@users.noreply.github.com>
2024-06-16 20:46:30 +0900

    [#6] Hㅇ법

--- jpa-practice/src/main/java/com/example/jpapractice/comment/Comment.java
+++ jpa-practice/src/main/java/com/example/jpapractice/comment/Comment.java
@@ -31,6 +31,10 @@ public class Comment {
   @OneToMany(mappedBy = "parentComment")
   private List<Comment> subComments = new ArrayList<>();
 
+  @Builder.Default
+  @OneToMany(mappedBy = "comment")
+  private List<CommentLike> commentLikes = new ArrayList<>();
+
   @Override
   public boolean equals(Object o) {
     if (this == o) {
