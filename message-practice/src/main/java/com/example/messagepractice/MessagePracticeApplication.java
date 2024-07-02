e3c63d30bf3f12de66798efde45cb27a4b10fc2e
prebird <78974381+prebird@users.noreply.github.com>
2024-07-02 12:50:06 +0900

    [#8] project init

---
+++ message-practice/src/main/java/com/example/messagepractice/MessagePracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.messagepractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagePracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagePracticeApplication.class, args);
+	}
+
+}
