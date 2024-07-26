d7239da32ea1816eb2b5caa847ed1fba9d5b9aaa
prebird <78974381+prebird@users.noreply.github.com>
2024-07-26 15:50:12 +0900

    [#9] stream cloud stream 연습을 위한 프로젝트 추가

---
+++ spring-cloud-stream-practice/src/main/java/org/prebird/springcloudstreampractice/SpringCloudStreamPracticeApplication.java
@@ -0,0 +1,13 @@
+package org.prebird.springcloudstreampractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudStreamPracticeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringCloudStreamPracticeApplication.class, args);
+  }
+
+}
