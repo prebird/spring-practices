fbf0f2248e799ade8c39dcac3d50301ccf1f2594
prebird <78974381+prebird@users.noreply.github.com>
2024-08-04 17:25:26 +0900

    [#10] spring proxy 구체 클래스인 경우 CGLIB proxy 만듬

---
+++ spring-practice/src/test/java/org/prebird/springpractice/common/ConcreteClass.java
@@ -0,0 +1,11 @@
+package org.prebird.springpractice.common;
+
+import lombok.extern.slf4j.Slf4j;
+
+// 구체 클래스
+@Slf4j
+public class ConcreteClass {
+  public void save() {
+    log.info("save called");
+  }
+}
--- spring-practice/src/test/java/org/prebird/springpractice/proxyFactory/ProxyFactoryTest.java
+++ spring-practice/src/test/java/org/prebird/springpractice/proxyFactory/ProxyFactoryTest.java
@@ -3,6 +3,7 @@ package org.prebird.springpractice.proxyFactory;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.prebird.springpractice.common.ConcreteClass;
 import org.prebird.springpractice.common.ServiceImpl;
 import org.prebird.springpractice.common.ServiceInterface;
 import org.springframework.aop.framework.ProxyFactory;
@@ -18,6 +19,21 @@ public class ProxyFactoryTest {
     ProxyFactory proxyFactory = new ProxyFactory(target);
     proxyFactory.addAdvice(new TimeAdvice());   // 프록시 로직
     ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+
+    log.info("target class = {}", target.getClass().getSimpleName());
+    log.info("proxy class = {}", proxy.getClass().getSimpleName());
+
+    proxy.save();
+  }
+
+  @Test
+  @DisplayName("구체클래스가 있으면 CGLIB 프록시")
+  public void concreteProxy() {
+    ConcreteClass target = new ConcreteClass();
+
+    ProxyFactory proxyFactory = new ProxyFactory(target);
+    proxyFactory.addAdvice(new TimeAdvice());   // 프록시 로직
+    ConcreteClass proxy = (ConcreteClass) proxyFactory.getProxy();
 
     log.info("target class = {}", target.getClass().getSimpleName());
     log.info("proxy class = {}", proxy.getClass().getSimpleName());
