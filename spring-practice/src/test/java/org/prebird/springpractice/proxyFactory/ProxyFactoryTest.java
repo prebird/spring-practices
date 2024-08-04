c0814008e78a513b49be75957323b3416b7b5288
prebird <78974381+prebird@users.noreply.github.com>
2024-08-04 17:28:45 +0900

    [#10] spring proxy proxyFactory 설정

--- spring-practice/src/test/java/org/prebird/springpractice/proxyFactory/ProxyFactoryTest.java
+++ spring-practice/src/test/java/org/prebird/springpractice/proxyFactory/ProxyFactoryTest.java
@@ -1,5 +1,7 @@
 package org.prebird.springpractice.proxyFactory;
 
+import static org.assertj.core.api.Assertions.*;
+
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -7,6 +9,7 @@ import org.prebird.springpractice.common.ConcreteClass;
 import org.prebird.springpractice.common.ServiceImpl;
 import org.prebird.springpractice.common.ServiceInterface;
 import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.aop.support.AopUtils;
 
 @Slf4j
 public class ProxyFactoryTest {
@@ -24,6 +27,9 @@ public class ProxyFactoryTest {
     log.info("proxy class = {}", proxy.getClass().getSimpleName());
 
     proxy.save();
+    assertThat(AopUtils.isAopProxy(proxy)).isTrue();
+    assertThat(AopUtils.isJdkDynamicProxy(proxy)).isTrue();
+    assertThat(AopUtils.isCglibProxy(proxy)).isFalse();
   }
 
   @Test
@@ -39,6 +45,27 @@ public class ProxyFactoryTest {
     log.info("proxy class = {}", proxy.getClass().getSimpleName());
 
     proxy.save();
+    assertThat(AopUtils.isAopProxy(proxy)).isTrue();
+    assertThat(AopUtils.isJdkDynamicProxy(proxy)).isFalse();
+    assertThat(AopUtils.isCglibProxy(proxy)).isTrue();
   }
 
+  @Test
+  @DisplayName("ProxyTargetClass 옵션을 설정하면 인터페이스가 있어도 CGLIB 기반 프록시를 만듬")
+  public void proxyTargetClass() {
+    ServiceInterface target = new ServiceImpl();
+
+    ProxyFactory proxyFactory = new ProxyFactory(target);
+    proxyFactory.addAdvice(new TimeAdvice());   // 프록시 로직
+    proxyFactory.setProxyTargetClass(true);   // CGLIB 기반 프록시로 만듬 설정
+    ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+
+    log.info("target class = {}", target.getClass().getSimpleName());
+    log.info("proxy class = {}", proxy.getClass().getSimpleName());
+
+    proxy.save();
+    assertThat(AopUtils.isAopProxy(proxy)).isTrue();
+    assertThat(AopUtils.isJdkDynamicProxy(proxy)).isFalse();
+    assertThat(AopUtils.isCglibProxy(proxy)).isTrue();
+  }
 }
